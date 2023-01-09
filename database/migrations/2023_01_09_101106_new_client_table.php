9714867b7ea615b72c70619d8960d847235fb599
Nils Fischer <nils@evergiven.doepke.local>
2023-01-09 21:05:14 +0100

    Migrations

---
+++ database/migrations/2023_01_09_101106_new_client_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('hostname')->nullable();
+            $table->integer('switch_id')->constrained('devices')->onDelete('cascade');
+            $table->integer('vlan_id');
+            $table->string('port_id');
+            $table->macAddress('mac_address');
+            $table->ipAddress('ip_address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
