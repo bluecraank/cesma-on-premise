5491e318d476c802a20bc1f7c722fcd34720224b
Nils Fischer <bluecraank@outlook.de>
2023-08-03 13:35:12 +0000

    Check if admin is needed

--- app/Http/Middleware/OnlyAllowAdmin.php
+++ app/Http/Middleware/OnlyAllowAdmin.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -16,6 +17,10 @@ class OnlyAllowAdmin
      */
     public function handle(Request $request, Closure $next)
     {
+        if(auth()->user()->role == 0 && User::count() === 1) {
+            auth()->user()->update(['role' => 2]);
+        }
+
         if(auth()->user()->role == 0) {
             return abort(403, 'Unauthorized action.');
         }
