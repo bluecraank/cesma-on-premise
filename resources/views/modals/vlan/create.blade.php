8c25bf1f66037af35f5ae6712b0242b77a095fb8
Nils Fischer <bluecraank@outlook.de>
2023-12-21 13:16:33 +0000

    Update Vlan create modal

--- resources/views/modals/vlan/create.blade.php
+++ resources/views/modals/vlan/create.blade.php
@@ -9,30 +9,21 @@
             <section class="modal-card-body">
                 <div class="field">
                     <input type="hidden" class="vlan-id" name="id">
-                    <label class="label">Name*</label>
-                    <p class="control has-icons-left">
+                    <label class="label">Name</label
+                    <p class="control">
                         <input class="input vlan-name" name="name" placeholder="Name des VLANs">
-                        <span class="icon is-small is-left">
-                            <i class="fa fa-a"></i>
-                        </span>
                     </p>
                 </div>
                 <div class="field">
                     <label class="label">ID</label>
-                    <p class="control has-icons-left">
+                    <p class="control">
                         <input class="input vlan-vid" type="number" max="4096" min="1" name="vid" placeholder="VLAN ID">
-                        <span class="icon is-small is-left">
-                            <i class="fa fa-info"></i>
-                        </span>
                     </p>
                 </div>
                 <div class="field">
                     <label class="label">{{ __('Description') }}</label>
-                    <p class="control has-icons-left">
+                    <p class="control">
                         <input class="input vlan-desc" name="description" placeholder="VLAN Beschreibung">
-                        <span class="icon is-small is-left">
-                            <i class="fa fa-info"></i>
-                        </span>
                     </p>
                 </div>
 
@@ -45,23 +36,20 @@
                     </div>
                 </div>
                 <div class="field">
-                    <label class="label">{{ __('Vlan.Subnet') }}</label>
-                    <p class="control has-icons-left">
+                    <label class="label">{{ __('Network range') }}</label>
+                    <p class="control">
                         <input class="input vlan-ip_range" name="ip_range" placeholder="10.10.10.0/24">
-                        <span class="icon is-small is-left">
-                            <i class="fa fa-up-down"></i>
-                        </span>
                     </p>
                 </div>
                 <div class="field">
                     <label class="checkbox">
                         <input type="checkbox" name="sync" class="vlan-sync">
-                        {{ __('Vlan.Sync') }}
+                        {{ __('Enable to sync this vlan') }}
                     </label>
                 </div>
             </section>
             <footer class="modal-card-foot">
-                <button class="button is-success">{{ __('Save') }}</button>
+                <button class="button is-success no-prevent">{{ __('Save') }}</button>
                 <button data-modal="create-vlan" type="button"
                     class="button">{{ __('Cancel') }}</button>
             </footer>
